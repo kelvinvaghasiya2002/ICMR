1b260e537ace113cc7d9352423379e16a5e64089
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-04-19 16:12:47 +0530

    m

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use(passport.session());
 
 app.use(function (req, res, next) {
     //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://icmr.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
