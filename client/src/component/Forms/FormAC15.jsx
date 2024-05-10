aac4e8b301f13131a4b1f11ec456f8011c05eac4
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-05-11 00:47:22 +0530

    html for form 15

--- client/src/component/Forms/FormAC15.jsx
+++ client/src/component/Forms/FormAC15.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { turnOffbutton } from './helpers'
 import DropDown from './child-comp/DropDown';
 import Checkbox from './child-comp/Checkbox';
@@ -6,6 +6,17 @@ import SidePanel from './child-comp/SidePanel';
 import { Link } from 'react-router-dom';
 
 function FormAC15() {
+    const [popup , setPopup] = useState(false);
+    function handleClick(event) {
+        const v= event.target.value;
+        if(v==="1" ||v==="2" ||v==="3" ||v==="4" ||v==="5" ||v==="6" ||v==="7" ||v==="8"){
+            setPopup(true)
+        }
+    }
+
+    function handleCancel(params) {
+        setPopup(false)
+    }
     turnOffbutton();
     return (
         <section id='Verbal-Social-Autopsy-Questionnaire-2'>
@@ -48,7 +59,7 @@ function FormAC15() {
 
                 <div className='block'>
                     <h3 className='h3block'>Place of death :</h3>
-                    <Checkbox CheckbobItems={["Hospital", "Home", "Workplace","In transit to seek care"]} name={"ac-33-3-6"} />
+                    <Checkbox CheckbobItems={["Hospital", "Home", "Workplace", "In transit to seek care"]} name={"ac-33-3-6"} />
                 </div>
 
                 <div className='block'>
@@ -63,18 +74,51 @@ function FormAC15() {
 
                 <div className='block'>
                     <h3 className='h3block'>What were the symptoms the deceased complained about? ( optional )</h3>
-                    <Checkbox
-                     CheckbobItems=
-                     {["STEMI : Acute chest pain, heaviness or constriction, or radiation to left arms or necks or back or breathlessness, or upper abdominal pain, or palpitation, or giddiness, which might have been accompanied by profuse sweating, and easily reproduced with post-meal exertion.",
-                      "Trauma & Burn : sudden injury in Road Traffic Accident/ severe fall/ drowning/ stabbing/ gunshot/ any other assault/ suicidal attempt/ burns/ domestic violence/ homicidal/ suicidal etc.",
-                       "Stroke: sudden onset of weakness, especially one side of the body/ loss of consciousness/ altered sensorium/ Imbalance/ blurred vision/ facial deviation/ drooping of eyelid/ speech abnormality with numbness and tingling sensation, or difficulty in speaking or understanding speech (aphasia), or sudden severe headache with no known cause of one's life (haemorrhagic strokes).",
-                       "Acute Respiratory Illness: sudden onset of fever, or cough with expectoration, or chest pain (pleuritic) , or fast breathing, or bluish colouration of tongue or lip (cyanosis), or speaks in short sentences, or altered mentation, chest tightness or pressure, indicating a lack of oxygen.", 
-                       "Postpartum Haemorrhage & Pre-Eclampsia: excessive bleeding or suddenly increased blood pressure or deterioration of sensorium or generalized seizure or fits etc after delivery.",
-                       "Neonatal Emergency: fever, or hypothermia, or convulsion, or any difficulty in feeding, or lethargy, or dusky colour, or cyanosis, or excessive diarrhoea, or jaundice, etc.",
-                       "Snake bite",
-                       "Poisoning: poisoning that leads to nausea, or vomiting, or diarrhoea, or drooling of saliva, or foaming at the mouth, or difficulty in breathing, or dilated or constricted pupils, or suddenly got faint, or seizure."]} 
-                     name={"ac-33-3-9"} />
+                    <form>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="1" name="ac-33-9" value="1" />
+                            <label style={{ fontWeight: "400" }} htmlFor="1">STEMI : Acute chest pain, heaviness or constriction, or radiation to left arms or necks or back or breathlessness, or upper abdominal pain, or palpitation, or giddiness, which might have been accompanied by profuse sweating, and easily reproduced with post-meal exertion.</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="2" name="ac-33-9" value="2" />
+                            <label style={{ fontWeight: "400" }} htmlFor="2">Trauma & Burn : sudden injury in Road Traffic Accident/ severe fall/ drowning/ stabbing/ gunshot/ any other assault/ suicidal attempt/ burns/ domestic violence/ homicidal/ suicidal etc.</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="3" name="ac-33-9" value="3" />
+                            <label style={{ fontWeight: "400" }} htmlFor="3">Stroke: sudden onset of weakness, especially one side of the body/ loss of consciousness/ altered sensorium/ Imbalance/ blurred vision/ facial deviation/ drooping of eyelid/ speech abnormality with numbness and tingling sensation, or difficulty in speaking or understanding speech (aphasia), or sudden severe headache with no known cause of one's life (haemorrhagic strokes).</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="4" name="ac-33-9" value="4" />
+                            <label style={{ fontWeight: "400" }} htmlFor="4">Acute Respiratory Illness: sudden onset of fever, or cough with expectoration, or chest pain (pleuritic) , or fast breathing, or bluish colouration of tongue or lip (cyanosis), or speaks in short sentences, or altered mentation, chest tightness or pressure, indicating a lack of oxygen.</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="5" name="ac-33-9" value="5" />
+                            <label style={{ fontWeight: "400" }} htmlFor="5">Postpartum Haemorrhage & Pre-Eclampsia: excessive bleeding or suddenly increased blood pressure or deterioration of sensorium or generalized seizure or fits etc after delivery.</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="6" name="ac-33-9" value="6" />
+                            <label style={{ fontWeight: "400" }} htmlFor="6">Neonatal Emergency: fever, or hypothermia, or convulsion, or any difficulty in feeding, or lethargy, or dusky colour, or cyanosis, or excessive diarrhoea, or jaundice, etc.</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="7" name="ac-33-9" value="7" />
+                            <label style={{ fontWeight: "400" }} htmlFor="7">Snake bite</label><br />
+                        </div>
+                        <div>
+                            <input onClick={handleClick} type="radio" id="8" name="ac-33-9" value="8" />
+                            <label style={{ fontWeight: "400" }} htmlFor="8">Poisoning: poisoning that leads to nausea, or vomiting, or diarrhoea, or drooling of saliva, or foaming at the mouth, or difficulty in breathing, or dilated or constricted pupils, or suddenly got faint, or seizure.</label><br />
+                        </div>
+                    </form>
                 </div>
+
+                {popup && <div>
+                    <h3>Form</h3>
+                    <h3>Confirmation Message</h3>
+                    <p>Your response have been received. </p>
+                    <div>
+                        <button onClick={handleCancel}>cancel</button>
+                        <button><Link to="/formsac-verbal&socialautopsyquestionnaire-3">Submit</Link></button>
+                    </div>
+                </div>}
 
                 <button className='nextbtn'><Link style={{ color: "white" }} to="/formsac-verbal&socialautopsyquestionnaire">Next</Link></button>
             </div>
---
+++ client/src/component/Forms/FormAC16.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FormAC16() {
+  return (
+    <div>FormAC16</div>
+  )
+}
+
+export default FormAC16
