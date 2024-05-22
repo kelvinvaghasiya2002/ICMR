702665c8a5916f47f028d87149f0893e201c5f93
Jash Patel <www.jashpatel211@gmail.com>
2024-05-22 15:58:41 +0530

    HFAT-2 styling

--- client/src/component/Forms/HFAT-1/FormE.jsx
+++ client/src/component/Forms/HFAT-1/FormE.jsx
@@ -5,18 +5,38 @@ import Buttons from '../child-comp/Buttons';
 import Radio from '../child-comp/Radio';
 import InputField from '../child-comp/InputField';
 import E1 from './tables/E1';
+import E2 from './tables/E2';
 
 function FormE() {
 
-  const columns =[
+  const columns1 =[
     { key: 'Number1', label: 'Adult (> 18Years)', type: 'input'},
     { key: 'Number2', label: 'Pediatric', type: 'input' },
     { key: 'Number3', label: 'Brought dead', type: 'input' },
     { key: 'Number4', label: 'Death after arrival', type: 'input' },
     { key: 'Number5', label: 'MLC', type: 'input' },
   ]
-  const initialRows = [
+  const initialRows1 = [
     { Number1: '', Number2: '', Number3: '', Number4: '', Number5:''},
+];
+
+const columns2 = [
+  { key: 'Type', label: 'Type', type: 'text' },
+  { key: 'Attended', label: 'Attended (NA: data unavailable)', type: 'input' },
+  { key: 'Death', label: 'Death (NA: data unavailable)', type: 'input' },
+];
+
+// Define the initial rows
+const initialRows2 = [
+  { Type: 'MI', Attended: '', Death: '' },
+  { Type: 'Stroke', Attended: '', Death: '' },
+  { Type: 'Trauma & Burns', Attended: '', Death: '' },
+  { Type: 'Poisoning', Attended: '', Death: '' },
+  { Type: 'Snake Bite', Attended: '', Death: '' },
+  { Type: 'Maternal Emergencies-PPH', Attended: '', Death: '' },
+  { Type: 'Maternal Emergencies- Eclampsia', Attended: '', Death: '' },
+  { Type: 'Neonatal Emergencies', Attended: '', Death: '' },
+  { Type: 'Acute Respiratory Illness', Attended: '', Death: '' },
 ];
   return (<section>
     <SidePanel id={"5"} />
@@ -31,8 +51,10 @@ function FormE() {
       </div>
 
       <div className="formcontent">
-        <E1 columns={columns} initialRows={initialRows}/>
-        <h1>table</h1>
+        <h3>Numbers of Patients who Visited ED in Last One Month</h3>
+        <E1 columns={columns1} initialRows={initialRows1}/>
+        <h3>Numbers of Patients Attended in ED and Deaths in Last One Year (Jan - Dec 2023)</h3>
+        <E2 columns={columns2} initialRows={initialRows2}/>
         <Checkbox
           h3="Which of these emergency care services does your facility provide? (Select all that apply)"
           CheckbobItems={[
--- client/src/component/Forms/HFAT-1/FormI.jsx
+++ client/src/component/Forms/HFAT-1/FormI.jsx
@@ -4,8 +4,28 @@ import SidePanel from './SidePanelHFAT1';
 import Buttons from '../child-comp/Buttons';
 import Radio from '../child-comp/Radio';
 import InputField from '../child-comp/InputField';
+import I1 from './tables/I1';
 
 function FormI() {
+  const columns = [
+    { key: 'EmergencyCondition', label: 'Emergency Condition', type: 'text' },
+    { key: 'SOP', label: 'Have Specific SOP/STW', type: 'radio', options: ['Yes', 'No'] },
+    { key: 'FollowsSOP', label: 'Follows SOP', type: 'radio', options: ['Yes', 'No'] },
+];
+
+// Define the initial rows
+const initialRows = [
+    { EmergencyCondition: 'MI', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Stroke', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Trauma & Burns', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Poisoning', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Snake Bite', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Maternal Emergencies-PPH', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Maternal Emergencies- Eclampsia', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Neonatal Emergencies', SOP: '', FollowsSOP: '' },
+    { EmergencyCondition: 'Acute Respiratory Illness', SOP: '', FollowsSOP: '' },
+];
+
   return (
     <section>
       <SidePanel id={"9"} />
@@ -23,104 +43,8 @@ function FormI() {
 
           <Checkbox h3="Indicate whether your hospital has the following documented protocols and systems (Select all that apply):" CheckbobItems={["Procedure for registration and admission of new emergency patients", "Procedure/Policy for receiving of referral patients", "Emergency Manual at the point of care", "Triage guidelines and protocol.", "Discharge summaries for all patients.", "Policy on handling cases of death (outside and inside hospital).", "Disaster management plan", "Triage policy/system at your emergency department.", "Alert system: Code Blue.", "Alert system: Trauma.", "Alert system: Chest Pain", "Alert system: Sepsis.", "Alert system: Stroke", "Alert system: Maternal Emergencies", "Alert system: Neonatal Emergencies", "Alert system: Acute Respiratory Emergencies", "Alert system: Snake bite and Poisoning"]} name="1I-1" />
 
-          <div>
-
-              <h3>Whether having Emergency condition specific SOP/STW for emergency care? </h3>
-
-        
-
-
-          <table>
-            <tr>
-              <th>Emergency Condition</th>
-              <th>Have Specific SOP/STW</th>
-              <th>Follows SOP</th>
-            </tr>
-            <tr>
-            <td>Acute Respiratory Illness</td>
-              <td>
-               <label><input type="radio" name="1" value="3"></input>Yes</label>
-               <label><input type="radio" name="1" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="2" value="3"></input>Yes</label>
-              <label><input type="radio" name="2" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>MI</td>
-              <td><label><input type="radio" name="3" value="3"></input>Yes</label>
-              <label><input type="radio" name="3" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="4" value="3"></input>Yes</label>
-              <label> <input type="radio" name="4" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Stroke</td>
-              <td><label><input type="radio" name="5" value="3"></input>Yes</label>
-              <label><input type="radio" name="5" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="6" value="3"></input>Yes</label>
-              <label><input type="radio" name="6" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Trauma & Burns</td>
-              <td><label><input type="radio" name="7" value="3"></input>Yes</label>
-              <label> <input type="radio" name="7" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="8" value="3"></input>Yes</label>
-              <label><input type="radio" name="8" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Poisoning</td>
-              <td><label><input type="radio" name="9" value="3"></input>Yes</label>
-              <label><input type="radio" name="9" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="10" value="3"></input>Yes</label>
-              <label><input type="radio" name="10" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Snake Bite</td>
-              <td><label><input type="radio" name="11" value="3"></input>Yes</label>
-              <label><input type="radio" name="11" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="12" value="3"></input>Yes</label>
-              <label><input type="radio" name="12" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Maternal Emergencies-PPH</td>
-              <td><label><input type="radio" name="13" value="3"></input>Yes</label>
-              <label><input type="radio" name="13" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="14" value="3"></input>Yes</label>
-              <label><input type="radio" name="14" value="3"></input>No</label>
-              </td>
-            </tr>
-            <tr>
-            <td>Maternal Emergencies- Eclampsia</td>
-              <td><label><input type="radio" name="15" value="3"></input>Yes</label>
-              <label> <input type="radio" name="15" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="16" value="3"></input>Yes</label>
-              <label><input type="radio" name="16" value="3"></input>No</label>
-              </td>
-              </tr>
-            <tr>
-              <td>Acute Respiratory Illness</td>
-              <td><label><input type="radio" name="17" value="3"></input>Yes</label>
-              <label><input type="radio" name="17" value="3"></input>No</label>
-               </td>
-              <td><label><input type="radio" name="18" value="3"></input>Yes</label>
-              <label><input type="radio" name="19" value="3"></input>No</label>
-              </td>
-            </tr>
-          </table>
-
-          </div>
+          <h3>Whether having Emergency condition specific SOP/STW for emergency care? </h3>
+          <I1 columns={columns} initialRows={initialRows} />
 
           <Buttons prev="/leadershipandgovernance" next="/referrallinkages" />
         </div>
