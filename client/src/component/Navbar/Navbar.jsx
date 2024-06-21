204ce63f09bc005c41afe6a7e3ea8e079e67ab9e
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-06-22 03:36:41 +0530

    Changed design of NavBar

--- client/src/component/Navbar/Navbar.jsx
+++ client/src/component/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import './Navbar.css'
-import ICMRIcon from "../../assets/ICMR_Icon.png"
 import ICMRLogo from "../../assets/ICMR_Logo.png";
 import PUIcon from "../../assets/PU_Icon.png";
+import secondLogo from "../../assets/secondLogo.png"
+import thirdLogo from "../../assets/thirdLogo.png"
+import fourthLogo from "../../assets/fourthLogo.png"
+import fifthLogo from "../../assets/fifthLogo.png"
 import { useUserInfo } from '../../contexts/User';
 import { Link, NavLink } from 'react-router-dom';
 const VITE_SERVER = import.meta.env.VITE_SERVER;
@@ -32,24 +35,31 @@ export default function Navbar() {
     return (
         <>
             <div id='navigation' style={!loggedIn ? { boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" } : { boxShadow: 'none' }} className='navbar'>
-                <div className='ICMRIcon  nav-img'>
-                    <Link to="/"><img src={ICMRIcon}></img></Link>
+
+                <div className='ICMRLogo  nav-img'>
+                    <Link to="/"><img src={ICMRLogo}></img></Link>
                 </div>
-                <div className='ICMRLogo nav-img'>
-                    <img src={ICMRLogo} className=''></img>
+
+                <div className='secondLogo nav-img'>
+                    <img src={secondLogo}></img>
                 </div>
-                <div className={loggedIn ? 'PUIcon nav-img nav-flex' : 'PUIcon nav-img'}>
-                    {loggedIn ?
-                        <>
-                            <div><img src={PUIcon} className='ICMRIcon'></img></div>
-                            <div>
-                                <p>Welcome <span style={{ color: "#3177FF" }}>{user.name}</span></p>
-                                <p>SITE : <span style={{ fontWeight: "400" }}>{user.sitename}</span></p>
-                            </div>
-                        </> :
-                        <img src={PUIcon} className='ICMRIcon'></img>
-                    }
+
+                <div className='secondLogo nav-img'>
+                    <img src={thirdLogo}></img>
                 </div>
+
+                <div className='secondLogo nav-img'>
+                    <img src={fourthLogo}></img>
+                </div>
+
+                <div className='secondLogo nav-img'>
+                    <img src={fifthLogo}></img>
+                </div>
+
+                <div className={'PUIcon nav-img'}>
+                    <img src={PUIcon} className='ICMRIcon'></img>
+                </div>
+
             </div>
             {loggedIn && <>  <div id='nav-buttons' className='buttons-grid'>
                 <button id='but'><p>AIM</p></button>
