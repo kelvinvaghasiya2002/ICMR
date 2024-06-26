e20a2410349c971a5f57d6da69d9201942559eb3
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-06-26 15:52:43 +0530

    changes in cst forms

--- client/src/component/Forms/CST/FormA8.jsx
+++ client/src/component/Forms/CST/FormA8.jsx
@@ -10,8 +10,10 @@ import setLocalStorage from '../setLocalStorage.js';
 import Heading from '../../Heading/Heading.jsx';
 
 function FormA8() {
-  var forma8 = setLocalStorage("forma8", { AC8_1: "", AC8_1_if: "", AC8_2:"" })
-  const [formA8, setFormA8] = useState(JSON.parse(forma8))
+    var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: "", AC11_4: "", AC11_4_if: "", AC11_5: "", AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
+
+    const [formA3, setFormA3] = useState(JSON.parse(forma3))
+
   turnOffbutton();
   return (
     <div>
@@ -33,13 +35,13 @@ function FormA8() {
               <div className="formcontent cont_extra">
 
 
-                  <Radio onClick={handleChange(setFormA8)} h3="AC.8.1  In the past one year, has anyone in your household had a history of heart attack or sudden onset of acute chest pain/ heaviness/ constriction, with possible radiation to the left arm, neck, or back, associated with symptoms such as upper abdominal pain/ palpitations/ dizziness/ profuse sweating, and exacerbated by exertion after meals?" CheckbobItems={["Yes", "No"]} name="AC8_1" />
+                  <Radio onClick={handleChange(setFormA3)} h3="AC.8.1  In the past one year, has anyone in your household had a history of heart attack or sudden onset of acute chest pain/ heaviness/ constriction, with possible radiation to the left arm, neck, or back, associated with symptoms such as upper abdominal pain/ palpitations/ dizziness/ profuse sweating, and exacerbated by exertion after meals?" CheckbobItems={["Yes", "No"]} name="AC8_1" byDefault={formA3.AC8_1} />
 
-                  <InputField onChange={handleChange(setFormA8)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC8_1_if"  />
+                  <InputField onChange={handleChange(setFormA3)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC8_1_if" value={formA3.AC8_1_if}  />
 
-                  <DropDown onClick={handleChange(setFormA8)} className='dropdown' dropdownItems={["Bhagat Singh Bhavan", "C V Raman"]} name={"AC8_2"} h3="AC.8.2  If yes, could you please tell who all from your Household suffered with this condition?"/>
+                  <DropDown onClick={handleChange(setFormA3)} className='dropdown' dropdownItems={["Bhagat Singh Bhavan", "C V Raman"]} name={"AC8_2"} h3="AC.8.2  If yes, could you please tell who all from your Household suffered with this condition?" byDefault={formA3.AC8_2} />
 
-                  <Buttons prev="/burn" next="/stroke" prevText="Previous" nextText="Save & Next"/>
+                  <Buttons formName={"forma3"} formData={formA3} prev="/burn" next="/stroke" prevText="Previous" nextText="Save & Next"/>
               </div>
           </div>
       </section>
