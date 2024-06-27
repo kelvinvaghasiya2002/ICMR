95b6eb5c32048b325400b7bcedfe166b0c03ba75
hetashridk <hetashrik@gmail.com>
2024-06-27 14:34:57 +0530

    FormA4 to FormA7 state added

--- client/src/component/Forms/CST/FormA5.jsx
+++ client/src/component/Forms/CST/FormA5.jsx
@@ -12,6 +12,9 @@ import Heading from '../../Heading/Heading';
 import Table from '../child-comp/Table.jsx'
 
 function FormA5() {
+  var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: "", AC11_4: "", AC11_4_if: "", AC11_5: "", AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
+
+  const [formA3, setFormA3] = useState(JSON.parse(forma3))
   turnOffbutton();
   return (
     <div>
@@ -33,27 +36,30 @@ function FormA5() {
           <div className="formcontent cont_extra">
             <Radio
               name="AC3"
-              h3="In the past 1 year, did you or any member of this household suffer from sudden injury in Road Traffic Accident/severe fall/drowning/stabbing/gunshot/any other assault/suicidal attempt/burns/domestic violence/homicidal etc. that required immediate medical service?"
+              h3="I am reading out all the names of individuals residing in this house. Just to make sure that I have a complete household listing: Are there any other persons such as small children or infants that we have not listed?"
               CheckbobItems={["Yes", "No"]}
+              onClick={handleChange(setFormA3)}
+              byDefault={formA3.AC3}
             />
             <Radio
               name="AC4"
               h3="Are there any other people who may not be members of your family such as domestic servants who usually live here?"
               CheckbobItems={["Yes", "No"]}
+              onClick={handleChange(setFormA3)}
+              byDefault={formA3.AC4}
             />
-            
+
             <h4><i>Now, I would like to get some information related to the medical emergency conditions your household had been through in the past one year.</i></h4>
 
             <Radio
               name="AC5"
               h3="In the past one year, did any member of this household have any health emergency that could have required any sort of medical attention or treatment?"
               CheckbobItems={["Yes", "No"]}
+              onClick={handleChange(setFormA3)}
+              byDefault={formA3.AC5}
             />
 
-
-
-
-            <Buttons formName="formaa" prevText="Previous" nextText="Save & Next" prev="/linelistingofhouseholdmembers-2" next="/trauma" />
+            <Buttons formName="forma3" formData={formA3} prevText="Previous" nextText="Save & Next" prev="/linelistingofhouseholdmembers-2" next="/trauma" />
           </div>
         </div>
       </section>
--- client/src/component/Forms/CST/FormA7.jsx
+++ client/src/component/Forms/CST/FormA7.jsx
@@ -13,8 +13,9 @@ import Table from '../child-comp/Table.jsx'
 import DropDown from '../child-comp/DropDown.jsx';
 
 function FormA7() {
-  var forma7 = setLocalStorage("forma7", { AC7_1: "",AC7_1_if:"", AC7_2: "" })
-  const [formA7, setFormA7] = useState(JSON.parse(forma7))
+  var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: "", AC11_4: "", AC11_4_if: "", AC11_5: "", AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
+
+  const [formA3, setFormA3] = useState(JSON.parse(forma3))
   turnOffbutton();
   return (
     <div>
@@ -39,13 +40,15 @@ function FormA7() {
               name="AC7_1"
               h3="AC.7.1 In the past one year, did you or any member of this household suffer from severe/minor burns etc. that required medical attention?"
               CheckbobItems={["Yes", "No"]}
-              onClick={handleChange(setFormA7)}
+              onClick={handleChange(setFormA3)}
+              byDefault={formA3.AC7_1}
             />
-            <InputField h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC7_1_if" />
-            <DropDown h3="AC.7.2  If yes, could you please tell who all from your Household suffered with this condition?" dropdownItems={["< 1 year", "> 1 year"]} name="AC7_2" onClick={handleChange(setFormA7)} />
+            <InputField onClick={handleChange(setFormA3)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC7_1_if" value={formA3.AC7_1_if} />
+
+            <DropDown h3="AC.7.2  If yes, could you please tell who all from your Household suffered with this condition?" dropdownItems={["< 1 year", "> 1 year"]} name="AC7_2" onClick={handleChange(setFormA3)}  byDefault={formA3.AC7_2} />
 
 
-            <Buttons prev="/trauma" next="/stemi" prevText="Previous" nextText="Save & Next" />
+            <Buttons formName="forma3" formData={formA3}  prev="/trauma" next="/stemi" prevText="Previous" nextText="Save & Next" />
           </div>
         </div>
       </section>
