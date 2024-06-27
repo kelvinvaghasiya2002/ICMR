3792265d78a4f127948797d75215fa6c58195a2b
hetashridk <hetashrik@gmail.com>
2024-06-27 16:43:29 +0530

    skip part added in form 5,6,7

--- client/src/component/Forms/CST/FormA6.jsx
+++ client/src/component/Forms/CST/FormA6.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { handleChange, turnOffbutton } from '../helpers'
 import SidePanel from './SidePanelCST.jsx';
 import DropDown from '../child-comp/DropDown';
@@ -13,39 +13,52 @@ function FormA6() {
     var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: "", AC11_4: "", AC11_4_if: "", AC11_5: "", AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
 
     const [formA3, setFormA3] = useState(JSON.parse(forma3))
-  turnOffbutton();
-  return (
-    <div>
-    <Heading h2="Community Survey Tool"></Heading>
-      <section id='site-info'>
-          <SidePanel id={"6"} />
-          <div className='siteInfo'>
-              <div className="formhdr">
-                  <div>
-                      <h2>A Socio-demographic Characteristics</h2>
-                  </div>
-                  <div>
-                      <h3>
-                      Trauma
-                      </h3>
-                  </div>
-              </div>
 
-              <div className="formcontent cont_extra">
+    useEffect(() => {
+        if (formA3.AC6_1 === "No") {
+            setFormA3((prevValue) => {
+                return { ...prevValue, AC6_1_if: "", AC6_2: "" }
+            })
+        }
+    }, [formA3])
+    turnOffbutton();
+    return (
+        <div>
+            <Heading h2="Community Survey Tool"></Heading>
+            <section id='site-info'>
+                <SidePanel id={"6"} />
+                <div className='siteInfo'>
+                    <div className="formhdr">
+                        <div>
+                            <h2>A Socio-demographic Characteristics</h2>
+                        </div>
+                        <div>
+                            <h3>
+                                Trauma
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div className="formcontent cont_extra">
 
 
-                  <Radio onClick={handleChange(setFormA3)} h3="AC.6.1   In the past one year, did you or any member of this household suffered from sudden injury in Road Traffic Accident/ fracture/ severe fall/ drowning/ stabbing/ gunshot/ any other assault/ any attempt to self-harm/ domestic violence/ homicidal etc.?" CheckbobItems={["Yes", "No"]} name="AC6_1" byDefault={formA3.AC6_1} />
+                        <Radio onClick={handleChange(setFormA3)} h3="AC.6.1   In the past one year, did you or any member of this household suffered from sudden injury in Road Traffic Accident/ fracture/ severe fall/ drowning/ stabbing/ gunshot/ any other assault/ any attempt to self-harm/ domestic violence/ homicidal etc.?" CheckbobItems={["Yes", "No"]} name="AC6_1" byDefault={formA3.AC6_1} />
 
-                  <InputField onChange={handleChange(setFormA3)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC6_1_if" value={formA3.AC6_1_if} />
+                        {
+                            (formA3.AC6_1 === 'Yes') &&
+                            <>
+                                <InputField onChange={handleChange(setFormA3)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC6_1_if" value={formA3.AC6_1_if} />
 
-                  <DropDown onClick={handleChange(setFormA3)} className='dropdown' dropdownItems={["Bhagat Singh Bhavan", "C V Raman"]} name={"AC6_2"} h3="AC6.2   If yes, could you please tell who all from your Household suffered with this condition?" byDefault={formA3.AC6_2}/>
+                                <DropDown onClick={handleChange(setFormA3)} className='dropdown' dropdownItems={["Bhagat Singh Bhavan", "C V Raman"]} name={"AC6_2"} h3="AC6.2   If yes, could you please tell who all from your Household suffered with this condition?" byDefault={formA3.AC6_2} />
 
-                  <Buttons formName={"forma3"} formData={formA3}prev="/linelistingofhouseholdmembers-3" next="/burn" prevText="Previous" nextText="Save & Next"/>
-              </div>
-          </div>
-      </section>
-      </div>
-  )
+                            </>
+                        }
+                        <Buttons formName={"forma3"} formData={formA3} prev="/linelistingofhouseholdmembers-3" next="/burn" prevText="Previous" nextText="Save & Next" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default FormA6
