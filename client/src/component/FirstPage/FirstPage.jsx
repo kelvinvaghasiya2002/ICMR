35a67fd93155025f32843b2343873e4da407f04a
Purvi <purvi.v.malviya@gmail.com>
2024-06-27 11:32:42 +0530

    Updated the Homepage as per new UI

--- client/src/component/FirstPage/FirstPage.jsx
+++ client/src/component/FirstPage/FirstPage.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import heroImg from "../../assets/cuate.png";
 import './FirstPage.css'
 
 export default function FirstPage() {
@@ -7,32 +8,38 @@ export default function FirstPage() {
         <div className='fpage_main'>
 
             <div className='fpage_navbar'>
-                <div>
-                    <h1>Welcome</h1>
-                </div>
-                <div className='signin-up'>
-                    <Link to="/sign-up"><button className='signin-up-buttons xyz'>Sign Up</button></Link>
-                    <Link to="/sign-in"><button className='signin-up-buttons'>Sign In</button></Link>
-                </div>
-            </div>
 
-            <div className='morepage'>
-
+                <div className='morepage'>
                 <Link to='/'><button className='morepage-buttons'>Home</button></Link>
                 <Link to='/aboutus'><button className='morepage-buttons'>About Project</button></Link>
                 <Link to="/project-sites"><button className='morepage-buttons'>Project Sites</button></Link>
                 <Link to=""><button className='morepage-buttons'>Research Team</button></Link>
                 <Link to=""><button className='morepage-buttons'>What's New ?</button></Link>
+                </div>
+
+                <div className='signin-up'>
+                    <Link to="/sign-in"><button className='signin-up-buttons'>Sign In</button></Link>
+                    <Link to="/sign-up"><button className='signin-up-buttons xyz'>Sign Up</button></Link>
+                </div>
             </div>
 
-            <div className='fpage_content'>
-                <h3>
-                    An implementation research study<br /> on developing a High-quality patient centric integrated model <br />for emergency care systems in selected districts of India
-                </h3>
+
+            <div className='main_section'>
+                <div className='fpage_content'>
+                    <h2>
+                        An implementation research<br />study
+                    </h2>
+                    <p>a High-quality patient centric integrated model for<br />emergency care systems in selected districts of India</p>
+                    <div className='fpage_study'>
+                        <h3>AN ICMR TASK FORCE STUDY</h3>
+                    </div>
+                </div>
+
+                <div className='hero'>
+                    <img src={heroImg} alt="hero_image" />
+                </div>
             </div>
-            <div className='fpage_study'>
-                <h3>AN ICMR TASK FORCE STUDY</h3>
-            </div>
+           
             <div className='fpage_footer'>
                 <p>Content Managed By <span>ICMR-New Delhi</span></p>
                 <p style={{marginTop : "0px"}}>Design & Developed By <span>Parul <span style={{color : "red"}}>University</span></span></p>
