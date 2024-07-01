166abd6886f4b0a41ea16ca5a8d977d730144609
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-07-01 16:03:29 +0530

    Changed checkbox component

--- client/src/component/Forms/child-comp/Checkbox.jsx
+++ client/src/component/Forms/child-comp/Checkbox.jsx
@@ -1,40 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Checkbox({ CheckbobItems, name, h3, other, time, setFunction, StateValue, array }) {
     // console.log(name);
-    const [otherSpecify , setOtherSpecify] = useState("");
+    const [otherSpecify, setOtherSpecify] = useState("");
 
-    const handleChange = (event)=>{
+    useEffect(() => {
+        if (array.length === 0) {
+            array = CheckbobItems.map(() => "");
+        }
+        console.log(array);
+    }, [])
+
+    const handleChange = (event) => {
         setOtherSpecify(event.target.value);
-        array[0]=event.target.value;
+        array[array.length - 1] = event.target.value;
     }
 
-    const handleClick = (event) => {
-        const { value } = event.target;
-        if (array.includes(value)) {
-            console.log(value);
-            array = array.filter(item => {
-                return item !== value
-            })
-        } else {
-            array.push(value)
-        }
+    const handleClick = (index) => {
+        return (
+            (event) => {
+                const { value } = event.target;
+                if (array[index]==="") {
+                    // console.log(value);
+                    // array = array.filter(item => {
+                    //     return item !== value
+                    // })
+                    array[index] = value;
+                } else {
+                    // array.push(value)
+                    array[index] = "";
+                }
 
-        StateValue = {
-            ...StateValue,
-            [event.target.name]: array
-        }
-        setFunction(StateValue)
+                StateValue = {
+                    ...StateValue,
+                    [event.target.name]: array
+                }
+                setFunction(StateValue)
+            }
+        )
     }
 
-    const handleCheckboxClick = ()=>{
+    const handleCheckboxClick = () => {
         const otherSpecifyCheckBox = document.getElementById(`${name}otherSpecifyCheckBox`);
-        if(otherSpecifyCheckBox.checked == true){
+        if (otherSpecifyCheckBox.checked == true) {
             console.log(otherSpecifyCheckBox.checked);
             document.getElementById(name).disabled = false;
-        }else{
+        } else {
             setOtherSpecify("");
-            array[0]="";
+            array[0] = "";
             document.getElementById(name).disabled = true;
         }
     }
@@ -47,16 +60,16 @@ function Checkbox({ CheckbobItems, name, h3, other, time, setFunction, StateValu
                     {
                         CheckbobItems.map((item, index) => {
                             return (
-                                <div key={index} style={{ display: "flex", alignItems: "flex-start", marginBottom:"0.8vw" }}>
+                                <div key={index} style={{ display: "flex", alignItems: "flex-start", marginBottom: "0.8vw" }}>
 
                                     {
                                         (array.includes(item)) ?
-                                            <input onChange={handleClick} type="checkbox" id={item} name={name} value={item} checked />
+                                            <input onChange={handleClick(index)} type="checkbox" id={item} name={name} value={item} checked />
                                             :
-                                            <input onChange={handleClick} type="checkbox" id={item} name={name} value={item} />
+                                            <input onChange={handleClick(index)} type="checkbox" id={item} name={name} value={item} />
                                     }
 
-                                    <label style={{ fontSize:"1.1vw" ,fontWeight: "400", marginLeft: "0.25vw", color:"gray" }} htmlFor={item}>{item}</label>
+                                    <label style={{ fontSize: "1.1vw", fontWeight: "400", marginLeft: "0.25vw", color: "gray" }} htmlFor={item}>{item}</label>
 
                                     <br />
                                     {
@@ -76,9 +89,9 @@ function Checkbox({ CheckbobItems, name, h3, other, time, setFunction, StateValu
                         other &&
                         <>
                             <input id={`${name}otherSpecifyCheckBox`} onChange={handleCheckboxClick} value={otherSpecify} type="checkbox" name="checkbox" />
-                            <span style={{fontSize : "1.1vw", color:"gray", paddingLeft:"0.2vw"}}>Other (Specify)</span>
+                            <span style={{ fontSize: "1.1vw", color: "gray", paddingLeft: "0.2vw" }}>Other (Specify)</span>
                             <input
-                                className='others blockinput' onChange={handleChange} type="text" name="otherSpecify" value={array[0]} id={name} disabled/>
+                                className='others blockinput' onChange={handleChange} type="text" name="otherSpecify" value={array[0]} id={name} disabled />
                         </>
 
                     }
