310b8ad88fa2de04a780b86837f6914cda01b1c3
JashPatel2004 <www.jashpatel211@gmail.com>
2024-07-25 13:45:16 +0530

    cst structure ready

---
+++ client/src/component/Forms/CST/FormC20.jsx
@@ -0,0 +1,49 @@
+import SidePanel from './SidePanelCST.jsx';
+import Checkbox from '../child-comp/Checkbox.jsx';
+import Radio from '../child-comp/Radio.jsx';
+import { Link } from 'react-router-dom';
+import "../Form.css"
+import React, { useEffect, useState } from 'react'
+import Buttons from '../child-comp/Buttons.jsx';
+import InputField from '../child-comp/InputField.jsx';
+import { turnOffbutton, handleChange } from '../helpers.js';
+import setLocalStorage from '../setLocalStorage.js';
+import Heading from '../../Heading/Heading';
+import Table from '../child-comp/Table.jsx'
+import DropDown from '../child-comp/DropDown.jsx';
+import Table1 from '../child-comp/Table1.jsx';
+
+function FormC20() {
+    var formc20 = setLocalStorage("formc20", { B0: "", B0_d: "", B1: "", B2: "", B3: "", B4: "",  B5_dt: "", B6: "", B7: "", B8: "", B9: "", B10: "", B11_if: "", B12: "", B13: "", B14: "", B15: "", B16: "", B17_1: "",B17_2:"",B18:"", B19: "", B20: "", B21: "", B22_1: "",B22_2:"", B23_1: "",B23_2:"", B24: "", B25: "", B26: "", B27: "", B28: "", B29: "", B30: "", B31: "", B32: "", B33: "", B34: "" })
+    const [formC20, setFormC20] = useState(JSON.parse(formc20))
+    turnOffbutton();
+    return (
+        <div>
+            <Heading h2="Community Survey Tool"></Heading>
+            <section id='site-info'>
+                <SidePanel id={"20"} />
+                <div className='siteInfo'>
+                    <div className="formhdr">
+                        <div>
+                            <h2>A Socio-demographic Characteristics</h2>
+                        </div>
+                        <div>
+                            <h3>
+                                Initial Healthcare Seeking Pathway
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div className="formcontent cont_extra">
+                       
+
+                        <Buttons formName="formc20" formData={formc20} prev="/initialhealthcareseekingpathway-3" next="/referral-facility2" prevText="Previous" nextText="Save & Next" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+
+}
+
+export default FormC20
---
+++ client/src/component/Forms/CST/FormE26.jsx
@@ -0,0 +1,48 @@
+import SidePanel from './SidePanelCST.jsx';
+import Checkbox from '../child-comp/Checkbox.jsx';
+import Radio from '../child-comp/Radio.jsx';
+import { Link } from 'react-router-dom';
+import "../Form.css"
+import React, { useEffect, useState } from 'react'
+import Buttons from '../child-comp/Buttons.jsx';
+import InputField from '../child-comp/InputField.jsx';
+import { turnOffbutton, handleChange } from '../helpers.js';
+import setLocalStorage from '../setLocalStorage.js';
+import Heading from '../../Heading/Heading';
+import Table from '../child-comp/Table.jsx'
+import DropDown from '../child-comp/DropDown.jsx';
+import Table1 from '../child-comp/Table1.jsx';
+
+function FormE26() {
+    var forme26 = setLocalStorage("forme26", { B0: "", B0_d: "", B1: "", B2: "", B3: "", B4: "",  B5_dt: "", B6: "", B7: "", B8: "", B9: "", B10: "", B11_if: "", B12: "", B13: "", B14: "", B15: "", B16: "", B17_1: "",B17_2:"",B18:"", B19: "", B20: "", B21: "", B22_1: "",B22_2:"", B23_1: "",B23_2:"", B24: "", B25: "", B26: "", B27: "", B28: "", B29: "", B30: "", B31: "", B32: "", B33: "", B34: "" })
+    const [formE26, setFormE26] = useState(JSON.parse(forme26))
+    turnOffbutton();
+    return (
+        <div>
+            <Heading h2="Community Survey Tool"></Heading>
+            <section id='site-info'>
+                <SidePanel id={"26"} />
+                <div className='siteInfo'>
+                    <div className="formhdr">
+                        <div>
+                            <h2>A Socio-demographic Characteristics</h2>
+                        </div>
+                        <div>
+                            <h3>
+                                Initial Healthcare Seeking Pathway
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div className="formcontent cont_extra">
+                       
+
+                        <Buttons formName="forme26" formData={forme26} prev="/barriers-and-facilitators2" next="/verbal-&-social-autopsy1" prevText="Previous" nextText="Save & Next" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default FormE26
