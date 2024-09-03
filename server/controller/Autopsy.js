ff4862b5294bfa5326665dbae25b551f55b0b92f
Jay Gandhi <gandhijay126@gmail.com>
2024-09-03 08:21:43 +0530

    Autopsy Backend issue Sloved

--- server/controller/Autopsy.js
+++ server/controller/Autopsy.js
@@ -1,19 +1,25 @@
 import { Autopsy } from "../Database/Autopsy.js";
 
+const AutopsyConroller = (req, res) => {
+  var { completeform } = req.body;
 
-const AutopsyConroller = (req,res)=>{
-    const {CompleteForm} = req.body;
-    Autopsy.create(CompleteForm).then((response)=>{
-        res.status(200).json({
-            success : "Data submitted successfully!",
-            response : response
-        });
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).json({
-            error : "Error occured in saving data."
-        })
+  completeform = JSON.parse(completeform);
+
+  Autopsy.create(completeform)
+    .then((response) => {
+      res.status(200).json({
+        success: "Data submitted successfully!",
+        response: response,
+      });
     })
-}
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Error occured in saving data.",
+      });
+    });
+};
 
 export default AutopsyConroller;
+
+// TypeError: First argument to `Model` constructor must be an object, **not** a string. Make sure you're calling `mongoose.model()`, not `mongoose.Model()`.
