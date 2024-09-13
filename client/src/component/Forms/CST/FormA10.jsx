ae86497a03286b14664b0713055fdd4f4818247a
hetashridk <hetashrik@gmail.com>
2024-09-13 13:41:00 +0530

    Fix:Checkbox changed to Radio button in CST

--- client/src/component/Forms/CST/FormA10.jsx
+++ client/src/component/Forms/CST/FormA10.jsx
@@ -11,7 +11,7 @@ import Heading from '../../Heading/Heading.jsx';
 import Checkbox from '../child-comp/Checkbox.jsx';
 
 function FormA10() {
-    var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: [], AC7_1: "", AC7_1_if: "", AC7_2: [], AC8_1: "", AC8_1_if: "", AC8_2: [], AC9_1: "", AC9_1_if: "", AC9_2: [], AC10_1: "", AC10_1_if: "", AC10_2: [], AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: [], AC11_4: "", AC11_4_if: "", AC11_5: [], AC12_1: "", AC12_1_if: "", AC12_2: [], AC13_1: "", AC13_1_if: "", AC13_2: [], AC14_1: "", AC14_1_if: "", AC14_2: [], AC15_1: "", AC15_2: "", AC15_4: "" })
+    var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: [], AC11_4: "", AC11_4_if: "", AC11_5: [], AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
 
     const [formA3, setFormA3] = useState(JSON.parse(forma3))
 
@@ -39,7 +39,7 @@ function FormA10() {
     useEffect(() => {
         if (formA3.AC10_1 === "No") {
             setFormA3((prevValue) => {
-                return { ...prevValue, AC10_1_if: "", AC10_2: [] }
+                return { ...prevValue, AC10_1_if: "", AC10_2: "" }
             })
         }
     }, [formA3.AC10_1])
@@ -84,7 +84,7 @@ function FormA10() {
 
                                 {/* <DropDown onClick={handleChange(setFormA3)} className='dropdown' dropdownItems={["Bhagat Singh Bhavan", "C V Raman"]} name={"AC10_2"} h3="AC.10.2  If yes, could you please tell who all from your Household suffered with this condition?" byDefault={formA3.AC10_2} /> */}
 
-                                <Checkbox CheckbobItems={fetchCstTableDetail()} name={"AC10_2"} h3="AC.10.2  If yes, could you please tell who all from your Household suffered with this condition?"  setFunction={setFormA3} StateValue={formA3} array={formA3.AC10_2} />
+                                <Radio onClick={handleChange(setFormA3)} CheckbobItems={fetchCstTableDetail()} name={"AC10_2"} h3="AC.10.2  If yes, could you please tell who all from your Household suffered with this condition?" byDefault={formA3.AC10_2} />
                             </>
                         }
 
--- client/src/component/Forms/CST/FormA8.jsx
+++ client/src/component/Forms/CST/FormA8.jsx
@@ -11,7 +11,7 @@ import setLocalStorage from '../setLocalStorage.js';
 import Heading from '../../Heading/Heading.jsx';
 
 function FormA8() {
-    var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: [], AC7_1: "", AC7_1_if: "", AC7_2: [], AC8_1: "", AC8_1_if: "", AC8_2: [], AC9_1: "", AC9_1_if: "", AC9_2: [], AC10_1: "", AC10_1_if: "", AC10_2: [], AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: [], AC11_4: "", AC11_4_if: "", AC11_5: [], AC12_1: "", AC12_1_if: "", AC12_2: [], AC13_1: "", AC13_1_if: "", AC13_2: [], AC14_1: "", AC14_1_if: "", AC14_2: [], AC15_1: "", AC15_2: "", AC15_4: "" })
+    var forma3 = setLocalStorage("forma3", { AC1: "", AC2_1: "", AC3: "", AC4: "", AC5: "", AC6_1: "", AC6_1_if: "", AC6_2: "", AC7_1: "", AC7_1_if: "", AC7_2: "", AC8_1: "", AC8_1_if: "", AC8_2: "", AC9_1: "", AC9_1_if: "", AC9_2: "", AC10_1: "", AC10_1_if: "", AC10_2: "", AC11_1: "", AC11_1_if: "", AC11_2: "", AC11_2_if: "", AC11_3: [], AC11_4: "", AC11_4_if: "", AC11_5: [], AC12_1: "", AC12_1_if: "", AC12_2: "", AC13_1: "", AC13_1_if: "", AC13_2: "", AC14_1: "", AC14_1_if: "", AC14_2: "", AC15_1: "", AC15_2: "", AC15_4: "" })
 
     const [formA3, setFormA3] = useState(JSON.parse(forma3))
 
@@ -82,7 +82,7 @@ function FormA8() {
                             <>
                                 <InputField onChange={handleChange(setFormA3)} h3="If Yes, What were the symptoms of emergency conditions and first course of action?" placeholder="Type here" name="AC8_1_if" value={formA3.AC8_1_if} />
 
-                                <Checkbox CheckbobItems={fetchCstTableDetail()} name={"AC8_2"} h3="AC.8.2  If yes, could you please tell who all from your Household suffered with this condition?"  setFunction={setFormA3} StateValue={formA3} array={formA3.AC8_2} />
+                                <Radio onClick={handleChange(setFormA3)} CheckbobItems={fetchCstTableDetail()} name={"AC8_2"} h3="AC.8.2  If yes, could you please tell who all from your Household suffered with this condition?"  byDefault={formA3.AC8_2} />
 
                             </>
                         }
